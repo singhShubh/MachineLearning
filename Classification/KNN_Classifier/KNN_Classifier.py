96496aef05290b50378c343102c2d3f8d985df14
Shubham Singh <shubham.ece@outlook.com>
2017-02-10 10:08:28 +0530

    Update KNN_Classifier.py

--- Classification/KNN_Classifier/KNN_Classifier.py
+++ Classification/KNN_Classifier/KNN_Classifier.py
@@ -1,8 +1,11 @@
 import numpy as np
 from collections import Counter
+import warnings
 
 
 def k_nearest_neighbours(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less than total voting groups!
     distance = []
     for class_name in data:
         for features in data[class_name]:
